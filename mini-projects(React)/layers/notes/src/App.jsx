6c1939f2f5abc5d2e05c4f7fc871818d0757d201
Shoaib-Imrann <irexceed@gmail.com>
2024-09-26 00:43:15 +0530

    Added a new project

---
+++ mini-projects(React)/layers/notes/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './components/Background'
+import Background from './components/Background'
+import Foreground from './components/Foreground'
+
+function App() {
+  return (
+    <div className='relative w-full h-screen'>
+      <Background/>
+      <Foreground/>
+    </div>
+  )
+}
+
+export default App
